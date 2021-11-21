5f50faa0e8fd57e7d90eb45f03b58700f567eece
supare <supare@ncsu.edu>
2021-11-21 16:56:49 -0500

    Added edit_event documentation

--- src/functionality/edit_event.py
+++ src/functionality/edit_event.py
@@ -1,11 +1,24 @@
-
-
-
-
+import re
+import datetime
+import discord
+from discord import Client
+from discord.ext import commands
+from src.functionality.shared_functions import read_event_file, create_event_tree, delete_event_from_file
+from src.functionality.highlights import convert_to_12
 
 async def edit_event(ctx, bot):
     """
-    This function is used to edit an event.
+    Function:
+        edit_event
+    Description:
+        A existing event is edited from the user's schedule file
+    Input:
+        ctx: the current context
+        client: the instance of the bot
+    Output:
+        - A reply saying whether the event was updated or not
     """
-    pass
+
+    channel = await ctx.author.create_dm()
+    
 
