c9a0527fa2e042f39b072aacd71c671264c8fd79
supare <supare@ncsu.edu>
2021-11-21 22:28:10 -0500

    Added successful update message

--- src/functionality/edit_event.py
+++ src/functionality/edit_event.py
@@ -71,10 +71,12 @@ async def edit_event(ctx, client):
     else:
         eventFlag = True
         embed = discord.Embed(description="Looks like your schedule is empty. You can add events using the `!schedule` command!", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+        embed.set_footer(text=f"Requested by {ctx.author}")
         await ctx.send(embed=embed) 
     #delete the event and event type
     if not eventFlag:
         embed = discord.Embed(description="Please enter the name of the event you want to edit or exit update using `exitupdate`", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+        embed.set_footer(text=f"Requested by {ctx.author}")
         await ctx.send(embed=embed)
         selected_event = None
         while selected_event == None:
@@ -82,16 +84,19 @@ async def edit_event(ctx, client):
             event_msg = event_msg.content  # Strips message to just the text the user entered
             if event_msg.lower() == 'exitupdate':
                 embed = discord.Embed(description="`!editevent` command exited successfully!", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                embed.set_footer(text=f"Requested by {ctx.author}")
                 await ctx.send(embed=embed) 
                 break
             selected_event = next((item for item in events if item["name"] == event_msg), None)
             if selected_event == None:
                 embed = discord.Embed(description="Looks like you entered event name that does not exists in your schedule. Please enter a valid event name or exit by entering `exitupdate`", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                embed.set_footer(text=f"Requested by {ctx.author}")
                 await ctx.send(embed=embed)
                 
         if selected_event:
             valid_update = False
             embed = discord.Embed(description="Please enter the updated event information in following format:\n\n`" + json.dumps(selected_event) + "`\n\nor enter `exitupdate` to exit update", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+            embed.set_footer(text=f"Requested by {ctx.author}")
             await ctx.send(embed=embed)
             while not valid_update:
                 try:
@@ -105,12 +110,16 @@ async def edit_event(ctx, client):
                     valid_update = True
                 except ValueError:
                     embed = discord.Embed(description="Please enter valid updated event information in following format:\n\n`" + json.dumps(selected_event) + "`\n\nor enter `exitupdate` to exit update", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                    embed.set_footer(text=f"Requested by {ctx.author}")
                     await ctx.send(embed=embed)
             if valid_update:
+                updated_event_name = updated_event['name']
                 updated_event = Event(updated_event['name'], datetime.datetime.strptime(updated_event['startDateTime'], '%Y-%m-%d %H:%M:%S'),  datetime.datetime.strptime(updated_event['endDateTime'], '%Y-%m-%d %H:%M:%S'), updated_event['priority'], updated_event['type'], updated_event['notes'])
                 update_event_from_file(str(ctx.author.id), selected_event, updated_event)
-                embed = discord.Embed(description="Event `" + updated_event['name'] + "` updated successfully!", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                embed = discord.Embed(description="Event `" + updated_event_name + "` updated successfully!", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                embed.set_footer(text=f"Requested by {ctx.author}")
                 await ctx.send(embed=embed)
             else:
                 embed = discord.Embed(description="`!editevent` command exited successfully!", colour=discord.Colour.dark_red(), timestamp=ctx.message.created_at)
+                embed.set_footer(text=f"Requested by {ctx.author}")
                 await ctx.send(embed=embed)    
